7d95812e398ea59f1cf33cf6326f225e6dbd18ed
Mathusha <mathusinam@gmail.com>
2022-05-18 19:46:44 +0530

    Updated Exercise

--- Exercise.go
+++ Exercise.go
@@ -20,4 +20,23 @@ func main() {
 
 	fmt.Println("Exercise 1 is done!")
 	fmt.Println("Code changes aren't working!")
-}
+
+	inputNo03 := 20
+	inputNo04 := 30
+	inputNo05 := 40
+
+	if (inputNo03 > inputNo04) {
+
+		if (inputNo03 > inputNo05) {
+			fmt.Println("inputNo03 is the greatest integer")
+		} else {
+			fmt.Printf("%v is the largest no!", inputNo05)
+		} 
+	} else {
+		if (inputNo03 > inputNo05) {
+			fmt.Printf("%v is the greatest no", inputNo04)
+		} else {
+			fmt.Printf("%v is the greatest no and its %v", inputNo05, 40)
+		}
+	}
+} 
