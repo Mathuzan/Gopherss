86cbc5b2854da111017af0e79d41f660afc87773
Mathusha <mathusinam@gmail.com>
2022-05-19 19:35:37 +0530

    First TicketBooking software

---
+++ TicketBooking.go
@@ -0,0 +1,43 @@
+//Package main begins
+package main
+
+//Import the packages
+import (
+	"fmt"
+)
+
+//Function main begins
+func main() {
+	conferenceName := "Go Conference"
+	const ConferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.\n", ConferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend!")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	//ask user for their name
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter your Ticket count: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, ConferenceTickets)
+
+
+}
