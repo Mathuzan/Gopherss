0c3bbe55208169fad5affa99a9ad224f9ed977c8
Mathusha <mathusinam@gmail.com>
2022-05-19 06:37:29 +0530

    Formatted print

---
+++ third.go
@@ -0,0 +1,19 @@
+//Package main begins
+package main
+
+// Import packages
+import (
+	"fmt"
+)
+
+//Main function begins
+func main() {
+
+	third := 1.0 / 3
+	fmt.Println(third)
+	fmt.Printf("%v\n", third)
+	fmt.Printf("%f\n", third)
+	fmt.Printf("%3f\n", third)
+	fmt.Printf("%4.2f\n", third)
+	 
+}
