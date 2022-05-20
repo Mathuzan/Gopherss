acbc0106bb12ca98937948b291e6796040a5b281
Mathusha <mathusinam@gmail.com>
2022-05-20 21:08:41 +0530

    with a working - Simple Web Server with Golang

--- Go-Server/main.go
+++ Go-Server/main.go
@@ -7,43 +7,38 @@ import (
 	"net/http"
 )
 
-func formhandler(w http.ResponseWriter, r "http.Request") {
+func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
-		fmt.Printf(w, "ParseForm() err: %v", err)
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "Post request Successful")
-	name := r.formValue("name")
-	address := r.formValue("address")
-	fmt.Fprintf(w, "Name = %s\n", &name)
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
 	fmt.Fprintf(w, "Address = %s\n", address)
-
 }
 
-func helloHandler(w http.ResponseWriter, r "http.Request") {
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found", http.statusNotFound)
+		http.Error(w, "404 not found", http.StatusNotFound)
 		return
-	} 
-
+	}
 	if r.Method != "GET" {
-		http.Error(w, "Method is not supported", http.StatusNotFound)
+		http.Error(w, "method is not supported", http.StatusNotFound)
 		return
 	}
 	fmt.Fprintf(w, "hello!")
 }
 
-//Main function begins
 func main() {
-
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formhandler)
+	http.HandleFunc("/form", formHandler)
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err !=nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-
 }
