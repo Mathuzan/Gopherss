de3893461586c71d0a1c99a0a7fde5c552fcd438
Mathushan <Mathuzan97@gmail.com>
2022-06-10 15:55:44 +0530

    deap Revision

--- revision.go
+++ revision.go
@@ -6,8 +6,12 @@ import(
 
 func main() {
 
-	fmt.Println("Welcome our users!")
-	fmt.Println("Welcome to our Conference booking application")
+	var conferenceName = "Go Conference"
+	const ConferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Println("Welcome to", conferenceName, "our Conference booking application")
 	fmt.Println("Get your tickets here to attend!")
-	
+	fmt.Printf("We have total of %v tickets and %v are still avaialble!", ConferenceTickets, remainingTickets)
+
 }
