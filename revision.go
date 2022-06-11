0f190e634a98fd392754b787fa860c9f865d337e
Mathushan <Mathuzan97@gmail.com>
2022-06-11 17:11:38 +0530

    Added Code

--- revision.go
+++ revision.go
@@ -6,12 +6,15 @@ import(
 
 func main() {
 
-	var conferenceName = "Go Conference"
-	const ConferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const ConferenceTickets int = 50
+	var remainingTickets int = 50
+
+	var UserName string
+	UserName = "Tom"
 
 	fmt.Println("Welcome to", conferenceName, "our Conference booking application")
 	fmt.Println("Get your tickets here to attend!")
 	fmt.Printf("We have total of %v tickets and %v are still avaialble!", ConferenceTickets, remainingTickets)
-
+	fmt.Println(UserName)
 }
