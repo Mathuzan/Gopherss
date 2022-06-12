5c7cf771cca0ecdf64277436165b522b914ba619
Mathushan <Mathuzan97@gmail.com>
2022-06-12 09:25:06 +0530

    Starting Tutorial

---
+++ techWithTom/Tutorial.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to my quiz game!")
+
+	var name string = "Tim"
+	age := 21
+	fmt.Printf("Hello %v, how are you doing? You are %v", name, age)
+
+	
+}
